9e0bfab0373bd961fcec76afb7fbae5949fec41b
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-11 17:22:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,7 +68,7 @@ class Owner
   end
   
   def list_pets
-    puts "I have #{self.dogs.count} dog(s), and #{Cats.count} cat(s)."
+    puts "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
   
 end
